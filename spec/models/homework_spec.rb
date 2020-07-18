eac6e4e80216b00262296c5dcd4f1565f11f954c
JeongWooYeong <wjd030811@gmail.com>
2020-07-18 22:17:54 +0900

    [ADD] model "homework" relation test

--- spec/models/homework_spec.rb
+++ spec/models/homework_spec.rb
@@ -1,5 +1,47 @@
 require 'rails_helper'
 
 RSpec.describe Homework, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'all homework relationship verification' do
+    it 'has many homework files' do
+      relationship = Homework.reflect_on_association(:homework_files).macro
+      expect(relationship).to eql(:has_many)
+    end
+
+    it 'has many single files' do
+      relationship = Homework.reflect_on_association(:single_files).macro
+      expect(relationship).to eql(:has_many)
+    end
+
+    it 'has many multi files' do
+      relationship = Homework.reflect_on_association(:multi_files).macro
+      expect(relationship).to eql(:has_many)
+    end
+
+    it 'has many experiment files' do
+      relationship = Homework.reflect_on_association(:experiment_files).macro
+      expect(relationship).to eql(:has_many)
+    end
+
+    it 'has one excel files' do
+      relationship = Homework.reflect_on_association(:excel_file).macro
+      expect(relationship).to eql(:has_one)
+    end
+  end
+
+  describe 'only team homework relationship verification' do
+    it 'has many teams' do
+      relationship = Homework.reflect_on_association(:teams).macro
+      expect(relationship).to eql(:has_many)
+    end
+
+    it 'has many self evaluations' do
+      relationship = Homework.reflect_on_association(:self_evaluations).macro
+      expect(relationship).to eql(:has_many)
+    end
+
+    it 'has many mutual evaluations' do
+      relationship = Homework.reflect_on_association(:mutual_evaluations).macro
+      expect(relationship).to eql(:has_many)
+    end
+  end
 end
