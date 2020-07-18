0f97151abeb44f3b061e6feb65e7f40fe8a9a4c7
JeongWooYeong <wjd030811@gmail.com>
2020-07-18 22:21:25 +0900

    [ADD] model "member" relation test

--- spec/models/member_spec.rb
+++ spec/models/member_spec.rb
@@ -1,5 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe Member, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'relationship verification' do
+    it 'belongs to student' do
+      relationship = Member.reflect_on_association(:student).macro
+      expect(relationship).to eql(:belongs_to)
+    end
+
+    it 'belongs to team' do
+      relationship = Member.reflect_on_association(:team).macro
+      expect(relationship).to eql(:belongs_to)
+    end
+  end
 end
