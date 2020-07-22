687aa5b4950c15772330af4a32a5d8ffab4fa761
JeongWooYeong <wjd030811@gmail.com>
2020-07-22 22:36:55 +0900

    [FIX] text "homework" to "assignment"

--- spec/models/excel_file_spec.rb
+++ spec/models/excel_file_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe ExcelFile, type: :model do
   describe 'relationship verification' do
-    it 'belongs to homework' do
+    it 'belongs to assignment' do
       relationship = ExcelFile.reflect_on_association(:assignment).macro
       expect(relationship).to eql(:belongs_to)
     end
