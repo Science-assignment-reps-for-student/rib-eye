359e6fbbd908c56ceb9a6ebdbd78feaf69776b5e
JeongWooYeong <wjd030811@gmail.com>
2020-07-22 22:34:35 +0900

    [ADD] model "experiment_file" relation test, belongs to assignment

--- spec/models/experiment_file_spec.rb
+++ spec/models/experiment_file_spec.rb
@@ -2,6 +2,11 @@ require 'rails_helper'
 
 RSpec.describe ExperimentFile, type: :model do
   describe 'relationship verification' do
+    it 'belongs to assignment' do
+      relationship = ExperimentFile.reflect_on_association(:assignment).macro
+      expect(relationship).to eql(:belongs_to)
+    end
+
     it 'belongs to student' do
       relationship = ExperimentFile.reflect_on_association(:student).macro
       expect(relationship).to eql(:belongs_to)
