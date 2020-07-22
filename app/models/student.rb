fc20dba40b1884f2f4c23e68de3800727993322f
JeongWooYeong <wjd030811@gmail.com>
2020-07-22 22:22:06 +0900

    [ADD] helper method "class_number"

--- app/models/student.rb
+++ app/models/student.rb
@@ -2,4 +2,8 @@ class Student < ApplicationRecord
   has_many :personal_files, dependent: :delete_all
   has_many :self_evaluations, dependent: :delete_all
   has_many :mutual_evaluations, foreign_key: :target_id, dependent: :delete_all
+
+  def class_number
+    student_number[1]
+  end
 end
