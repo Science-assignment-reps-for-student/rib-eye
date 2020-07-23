57abb2ef72c4478173e96d9d92d0473f5ae1df9a
JeongWooYeong <wjd030811@gmail.com>
2020-07-23 15:57:50 +0900

    [ADD] consistent path configuare

--- app/models/excel_file.rb
+++ app/models/excel_file.rb
@@ -2,6 +2,6 @@ class ExcelFile < ApplicationRecord
   belongs_to :assignment
 
   def stored_dir
-    super + "/excel_file/#{assignment_id}"
+    super + "excel_file/#{assignment_id}/"
   end
 end
--- app/models/team_file.rb
+++ app/models/team_file.rb
@@ -3,11 +3,11 @@ class TeamFile < ApplicationRecord
   belongs_to :team
 
   def stored_dir
-    super + "/team_file/#{assignment_id}/#{team_id}"
+    super + "team_file/#{assignment_id}/#{team_id}/"
   end
 
   def singular_file_name(extension)
-    "[#{assignment.type_korean}][#{assignment.title}]"\
-    "#{team.student.class_number}반_#{team.name}" + extension
+    "'[#{assignment.type_korean}][#{assignment.title}]"\
+    "#{team.student.class_number}반_#{team.name}" + extension + "'"
   end
 end
