86ae5c916cdfb5e4f29f6acd4d04312fd7c34f47
JeongWooYeong <wjd030811@gmail.com>
2020-07-23 19:03:12 +0900

    [ADD] test storing file

--- spec/models/team_file_spec.rb
+++ spec/models/team_file_spec.rb
@@ -1,6 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe TeamFile, type: :model do
+  before(:all) do
+    @assignment = create(:assignment, type: 'TEAM')
+    @student = create(:student)
+    @team_file = create(:experiment_file,
+                        assignment_id: @assignment.id,
+                        student_id: @student.id)
+  end
+
   describe 'relationship verification' do
     it 'belongs to assignment' do
       relationship = TeamFile.reflect_on_association(:assignment).macro
@@ -10,6 +18,16 @@ RSpec.describe TeamFile, type: :model do
     it 'belongs to team' do
       relationship = TeamFile.reflect_on_association(:team).macro
       expect(relationship).to eql(:belongs_to)
+    end
+  end
+
+  describe 'file process' do
+    it 'storing singular file' do
+      singular_file_test(@team_file)
+    end
+
+    it 'storing plural files' do
+      plural_files_test(@team_file)
     end
   end
 end
