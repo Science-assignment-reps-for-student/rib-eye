a43720cb957d260effd67efaefc82d87b23c6b16
JeongWooYeong <wjd030811@gmail.com>
2020-07-23 15:59:30 +0900

    [ADD] test helper methods

---
+++ spec/models/model_spec_helper.rb
@@ -0,0 +1,22 @@
+require 'rails_helper'
+
+def singular_file_test(instance)
+  file = File.open(ApplicationRecord.stored_dir + '/.keep')
+  instance.store_singular_file(file)
+  FileUtils.mv(instance.stored_dir + instance.singular_file_name,
+               ApplicationRecord.stored_dir + '/.keep')
+  FileUtils.rm_rf("#{ApplicationRecord.stored_dir}"\
+                      "#{instance.assignment.type_korean.downcase}"\
+                      '_file')
+end
+
+def plural_files_test(instance)
+  files = [File.open(ApplicationRecord.stored_dir + '/.keep')]
+  instance.store_plural_files(*files)
+  files.each do |file|
+    FileUtils.mv(instance.stored_dir + File.basename(file), ApplicationRecord.stored_dir + '/.keep')
+  end
+  FileUtils.rm_rf("#{ApplicationRecord.stored_dir}"\
+                      "#{instance.assignment.type_korean.downcase}"\
+                      '_file')
+end
