f8788583bb8c21ca4548517bd9501236990dcd38
JeongWooYeong <wjd030811@gmail.com>
2020-07-23 16:01:21 +0900

    [FIX] path

--- app/models/personal_file.rb
+++ app/models/personal_file.rb
@@ -3,11 +3,11 @@ class PersonalFile < ApplicationRecord
   belongs_to :student
 
   def stored_dir
-    super + "/personal_file/#{assignment_id}/#{student_id}"
+    super + "personal_file/#{assignment_id}/#{student_id}/"
   end
 
   def singular_file_name(extension)
-    "[#{assignment.type_korean}][#{assignment.title}]"\
-    "#{student.student_number}_#{student.name}" + extension
+    "'[#{assignment.type_korean}][#{assignment.title}]"\
+    "#{student.student_number}_#{student.name}" + extension + "'"
   end
 end
