db80b63cd1989d1c252e38815259227e1daef5f0
JeongWooYeong <wjd030811@gmail.com>
2020-07-27 16:14:05 +0900

    [FIX] file process unification

--- app/models/concerns/file_helper.rb
+++ app/models/concerns/file_helper.rb
@@ -1,37 +1,28 @@
 module FileHelper
   extend ActiveSupport::Concern
 
+  def store_file(file, name = singular_file_name)
+    path = File.join(stored_dir, name)
+
+    FileUtils.mkdir_p(stored_dir)
+    FileUtils.mv(file, path)
+
+    self.file_name = name
+    self.path = path
+    save
+  end
+
+  def destroy_file
+    FileUtils.rm(File.join(stored_dir, file_name))
+  end
+
   def destroy!
     super.destroy_file
   end
 
-  def store_plural_files(*files)
-    FileUtils.mkdir_p(stored_dir)
-    files.each { |file| store_file(file) }
-  end
-
-  def store_singular_file(file)
-    FileUtils.mkdir_p(stored_dir)
-    store_file(file, singular_file_name(File.extname(file)))
-  end
-
-  def store_file(file, file_name = File.basename(file))
-    FileUtils.mv(file, stored_dir + file_name)
-  end
-
-  def destroy_file
-    FileUtils.rm(path)
-  end
-
   module FileGenerator
-    def create!(files, **kwargs)
-      instance = super kwargs
-
-      if files.length == 1
-        instance.store_singular_file(files[0])
-      else
-        instance.store_plural_files(*files)
-      end
+    def create!(file, name = singular_file_name, **kwargs)
+      super(kwargs).store_file(file, name)
     end
   end
 end
