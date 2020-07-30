2290fdd7132ca2d5f022e5b996c9f9553a411859
JeongWooYeong <wjd030811@gmail.com>
2020-07-30 23:14:05 +0900

    [ADD] initialize personal file controller

--- spec/requests/personal_files_request_spec.rb
+++ spec/requests/personal_files_request_spec.rb
@@ -1,5 +1,26 @@
 require_relative 'request_spec_helper'
 
 RSpec.describe 'PersonalFiles', type: :request do
+  before(:all) do
+    set_database('PERSONAL')
 
+    @url_personal_file = URL_PREFIX + '/personal-file'
+    @url_personal_files = @url_personal_file + 's'
+  end
+
+  describe 'GET#show' do
+
+  end
+
+  describe 'GET#index' do
+
+  end
+
+  describe 'POST#create' do
+
+  end
+
+  describe 'DELETE#destroy' do
+
+  end
 end
