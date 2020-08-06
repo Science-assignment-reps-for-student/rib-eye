79d93fd4764b20ff1e50c4fc83770daef610f534
JeongWooYeong <wjd030811@gmail.com>
2020-08-06 09:19:50 +0900

    [ADD] personal file API

--- app/controllers/personal_files_controller.rb
+++ app/controllers/personal_files_controller.rb
@@ -2,7 +2,16 @@ class PersonalFilesController < ApplicationController
   before_action :jwt_required
 
   def show
+    params.require(%i[student_id assignment_id])
+    return render status: :forbidden unless current_admin
 
+    personal_file = PersonalFile.find_by_student_id_and_assignment_id(params[:student_id],
+                                                                      params[:assignment_id])
+    return render status: :not_found unless personal_file
+
+    send_file(personal_file.path,
+              filename: personal_file.file_name,
+              status: :no_content)
   end
 
   def index
