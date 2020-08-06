3e73d07afd57652347a66e76d4cc98a6b361998a
JeongWooYeong <wjd030811@gmail.com>
2020-08-06 09:18:49 +0900

    [ADD] show API test code

--- spec/requests/personal_files_request_spec.rb
+++ spec/requests/personal_files_request_spec.rb
@@ -13,7 +13,43 @@ RSpec.describe 'PersonalFiles', type: :request do
   end
 
   describe 'GET#show' do
+    it 'OK' do
+      request('get', @url_personal_file, { student_id: @student.id,
+                                           assignment_id: @assignment.id }, true)
+      expect(response.status).to equal(204)
+    end
 
+    it 'Unauthorized' do
+      request('get',
+              @url_personal_file,
+              { student_id: @student.id, assignment_id: @assignment.id },
+              false)
+      expect(response.status).to equal(401)
+    end
+
+    it 'Forbidden' do
+      request('get',
+              @url_personal_file,
+              { student_id: @student.id, assignment_id: @assignment.id },
+              JWT_BASE.create_refresh_token(sub: @student.email))
+      expect(response.status).to equal(403)
+    end
+
+    it 'Not Found student id' do
+      request('get',
+              @url_personal_file,
+              { student_id: @student.id + 1, assignment_id: @assignment.id },
+              true)
+      expect(response.status).to equal(404)
+    end
+
+    it 'Not Found assignment id' do
+      request('get',
+              @url_personal_file,
+              { student_id: @student.id, assignment_id: @assignment.id + 1 },
+              true)
+      expect(response.status).to equal(404)
+    end
   end
 
   describe 'GET#index' do
