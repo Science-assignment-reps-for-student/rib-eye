178b05f242000c9939c094508c0b967eade97da3
JeongWooYeong <wjd030811@gmail.com>
2020-08-06 09:09:24 +0900

    [ADD] store dummy file to path

--- spec/factories/experiment_files.rb
+++ spec/factories/experiment_files.rb
@@ -1,7 +1,12 @@
 FactoryBot.define do
   factory :experiment_file do
     file_name { '실험.hwp' }
-    path { '/rib-eye/experiment_file/1/실험.hwp' }
+    path do
+      path = File.join(ApplicationRecord.stored_dir, 'experiment_file/1/1/실험.hwp')
+      FileUtils.mkdir_p(File.dirname(path))
+      FileUtils.touch(path)
+      path
+    end
     created_at { DateTime.new(2020) }
     is_late { false }
   end
--- spec/factories/team_files.rb
+++ spec/factories/team_files.rb
@@ -1,7 +1,12 @@
 FactoryBot.define do
   factory :team_file do
     file_name { '팀.hwp' }
-    path { '/rib-eye/experiment_file/1/팀.hwp' }
+    path do
+      path = File.join(ApplicationRecord.stored_dir, 'experiment_file/1/팀.hwp')
+      FileUtils.mkdir_p(File.dirname(path))
+      FileUtils.touch(path)
+      path
+    end
     created_at { DateTime.new(2020) }
     is_late { false }
   end
