9edc55be157802688b5fd90a8aed5b52582427d9
JeongWooYeong <wjd030811@gmail.com>
2020-08-10 09:23:34 +0900

    [FIX] split token two type

--- spec/requests/request_spec_helper.rb
+++ spec/requests/request_spec_helper.rb
@@ -4,13 +4,13 @@ require 'jwt_base'
 JWT_BASE = JWTBase.new(ENV['SECRET_KEY_BASE'], 1.days, 2.weeks).freeze
 URL_PREFIX = '/rib-eye'.freeze
 
-def request(method, url, params = false, headers = false)
+def request(method, url, params = false, headers = true)
   parameters = {}
 
   parameters[:params] = params if params
 
   if headers == true
-    parameters[:headers] = { Authorization: "Bearer #{@token}" }
+    parameters[:headers] = { Authorization: "Bearer #{@admin_token}" }
   elsif headers
     parameters[:headers] = { Authorization: "Bearer #{headers}" }
   end
@@ -21,9 +21,12 @@ end
 def set_database(assignment_type)
   @student = create(:student)
   @admin = create(:admin)
-  @token = JWT_BASE.create_access_token(sub: @admin.email)
+
+  @student_token = JWT_BASE.create_access_token(sub: @student.email)
+  @admin_token = JWT_BASE.create_access_token(sub: @admin.email)
 
   @assignment = create(:assignment, type: assignment_type)
+
   @file = create("#{assignment_type.downcase}_file",
                  student_id: @student.id,
                  assignment_id: @assignment.id)
@@ -32,4 +35,6 @@ end
 def clean_dummy_file(assignment_type)
   FileUtils.rm_rf File.join(ApplicationRecord.stored_dir,
                             "#{assignment_type.downcase}_file")
+  FileUtils.rm_rf(File.join(ApplicationRecord.stored_dir, 'test.hwp'))
+  FileUtils.rm_rf(File.join(ApplicationRecord.stored_dir, 'test.unable'))
 end
