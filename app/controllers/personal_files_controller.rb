bd3e992917c05f6dc3c65f547228b0e098e01a9d
JeongWooYeong <wjd030811@gmail.com>
2020-08-10 09:18:36 +0900

    [FIX] pretreatment current admin, student, assignment

--- app/controllers/personal_files_controller.rb
+++ app/controllers/personal_files_controller.rb
@@ -1,9 +1,12 @@
 class PersonalFilesController < ApplicationController
   before_action :jwt_required
+  before_action :file_input_stream, only: :create
+  before_action :current_assignment, only: %i[create destroy]
+  before_action :current_student, only: %i[create destroy]
+  before_action :current_admin, only: :show
 
   def show
     params.require(%i[student_id assignment_id])
-    return render status: :forbidden unless current_admin
 
     personal_file = PersonalFile.find_by_student_id_and_assignment_id(params[:student_id],
                                                                       params[:assignment_id])
@@ -14,24 +17,35 @@ class PersonalFilesController < ApplicationController
               status: :no_content)
   end
 
-  def index
-    return render status: :forbidden unless current_admin
-
-    assignment = Assignment.find_by_id(params[:assignment_id])
-    return render status: :not_found unless assignment
-
-    assignment.generate_compressed_file
-
-    send_file(assignment.compressed_file_path,
-              filename: assignment.compressed_file_name,
-              status: :no_content)
-  end
-
   def create
+    if @student.personal_files.find_by_assignment_id(params[:assignment_id])
+      return render status: :conflict
+    end
 
+    if @files.length == 1
+      PersonalFile.create!(@files[0],
+                           student_id: @student.id,
+                           assignment_id: params[:assignment_id],
+                           created_at: Time.zone.now)
+    else
+      @files.each do |file|
+        PersonalFile.create!(file,
+                             student_id: @student.id,
+                             assignment_id: params[:assignment_id],
+                             file_name: File.basename(file),
+                             created_at: Time.zone.now)
+      end
+    end
+
+    render status: :created
   end
 
   def destroy
+    assignment = @student.personal_files.find_by_assignment_id(params[:assignment_id])
+    return render status: :precondition_failed unless assignment
 
+    assignment.destroy!
+
+    render status: :ok
   end
 end
