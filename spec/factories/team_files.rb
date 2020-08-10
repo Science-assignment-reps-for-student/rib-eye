2ce45deceaca315aadbb9317621c649cd944b8a6
JeongWooYeong <wjd030811@gmail.com>
2020-08-10 09:22:06 +0900

    [FIX] team file path

--- spec/factories/team_files.rb
+++ spec/factories/team_files.rb
@@ -2,7 +2,7 @@ FactoryBot.define do
   factory :team_file do
     file_name { '팀.hwp' }
     path do
-      path = File.join(ApplicationRecord.stored_dir, 'experiment_file/1/팀.hwp')
+      path = File.join(ApplicationRecord.stored_dir, 'team_file/1/1/팀.hwp')
       FileUtils.mkdir_p(File.dirname(path))
       FileUtils.touch(path)
       path
