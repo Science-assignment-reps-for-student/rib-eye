5ddd1271bd9218734285a22b1084a919a4a35c65
JeongWooYeong <wjd030811@gmail.com>
2020-08-10 16:34:19 +0900

    [FIX] use splited module method

--- app/controllers/personal_files_controller.rb
+++ app/controllers/personal_files_controller.rb
@@ -1,51 +1,40 @@
 class PersonalFilesController < ApplicationController
+  include FileScaffold::ControllerMethod
+  include FileScaffold::HelperMethod
+
   before_action :jwt_required
   before_action :file_input_stream, only: :create
-  before_action :current_assignment, only: %i[create destroy]
+  before_action :current_assignment, only: %i[create destroy index]
   before_action :current_student, only: %i[create destroy]
-  before_action :current_admin, only: :show
+  before_action :current_admin, only: %i[show index]
 
   def show
-    params.require(%i[student_id assignment_id])
+    super { PersonalFile.find_by_id(params[:file_id]) }
+  end
 
-    personal_file = PersonalFile.find_by_student_id_and_assignment_id(params[:student_id],
-                                                                      params[:assignment_id])
-    return render status: :not_found unless personal_file
+  def index
+    params.require(:student_id)
 
-    send_file(personal_file.path,
-              filename: personal_file.file_name,
-              status: :no_content)
+    student = Student.find_by_id(params[:student_id])
+    return render status: :not_found unless student
+
+    super do
+      PersonalFile.where(student: student,
+                         assignment: @assignment)
+    end
   end
 
   def create
-    if @student.personal_files.find_by_assignment_id(params[:assignment_id])
-      return render status: :conflict
+    super(ExperimentFile,
+          student_id: @student.id,
+          assignment_id: @assignment.id,
+          created_at: Time.zone.now) do
+      PersonalFile.find_by_student_id_and_assignment_id(@student.id,
+                                                        @assignment.id)
     end
-
-    if @files.length == 1
-      PersonalFile.create!(@files[0],
-                           student_id: @student.id,
-                           assignment_id: params[:assignment_id],
-                           created_at: Time.zone.now)
-    else
-      @files.each do |file|
-        PersonalFile.create!(file,
-                             student_id: @student.id,
-                             assignment_id: params[:assignment_id],
-                             file_name: File.basename(file),
-                             created_at: Time.zone.now)
-      end
-    end
-
-    render status: :created
   end
 
   def destroy
-    assignment = @student.personal_files.find_by_assignment_id(params[:assignment_id])
-    return render status: :precondition_failed unless assignment
-
-    assignment.destroy!
-
-    render status: :ok
+    super { @student.personal_files.where(assignment: @assignment) }
   end
 end
