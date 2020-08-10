da8207bc5e447baaf8e1da1b505dd117a468037a
JeongWooYeong <wjd030811@gmail.com>
2020-08-10 16:34:37 +0900

    [FIX] index route

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,20 @@
 Rails.application.routes.draw do
   scope(path: '/rib-eye') do
     # personal file controller
-    get '/personal-file', to: 'personal_files#show'
+    get '/personal-file/:file_id', to: 'personal_files#show'
+    get '/personal-file', to: 'personal_files#index'
     post '/personal-file/:assignment_id', to: 'personal_files#create'
     delete '/personal-file/:assignment_id', to: 'personal_files#destroy'
 
     # team file controller
-    get '/team-file', to: 'team_files#show'
+    get '/team-file/:file_id', to: 'team_files#show'
+    get '/team-file', to: 'team_files#index'
     post '/team-file/:assignment_id', to: 'team_files#create'
     delete '/team-file/:assignment_id', to: 'team_files#destroy'
 
     # experiment file controller
-    get '/experiment-file', to: 'experiment_files#show'
+    get '/experiment-file/:file_id', to: 'experiment_files#show'
+    get '/experiment-file', to: 'experiment_files#index'
     post '/experiment-file/:assignment_id', to: 'experiment_files#create'
     delete '/experiment-file/:assignment_id', to: 'experiment_files#destroy'
 
