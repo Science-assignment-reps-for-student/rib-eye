ff9059ff4eeb9598534f14e5e8546eb3f3bb9279
JeongWooYeong <wjd030811@gmail.com>
2020-08-17 01:27:51 +0900

    [FIX] routes to split assignment files

--- config/routes.rb
+++ config/routes.rb
@@ -2,34 +2,36 @@ Rails.application.routes.draw do
   scope(path: '/rib-eye') do
     # personal file controller
     get '/personal-file/:file_id', to: 'personal_files#show'
-    get '/personal-file', to: 'personal_files#index'
+    get '/personal-files/:assignment_id', to: 'personal_files#index'
     post '/personal-file/:assignment_id', to: 'personal_files#create'
-    delete '/personal-file/:assignment_id', to: 'personal_files#destroy'
+    delete '/personal-file/:file_id', to: 'personal_files#destroy'
 
     # team file controller
     get '/team-file/:file_id', to: 'team_files#show'
-    get '/team-file', to: 'team_files#index'
+    get '/team-files/:assignment_id', to: 'team_files#index'
     post '/team-file/:assignment_id', to: 'team_files#create'
-    delete '/team-file/:assignment_id', to: 'team_files#destroy'
+    delete '/team-file/:file_id', to: 'team_files#destroy'
 
     # experiment file controller
     get '/experiment-file/:file_id', to: 'experiment_files#show'
-    get '/experiment-file', to: 'experiment_files#index'
+    get '/experiment-files/:assignment_id', to: 'experiment_files#index'
     post '/experiment-file/:assignment_id', to: 'experiment_files#create'
-    delete '/experiment-file/:assignment_id', to: 'experiment_files#destroy'
+    delete '/experiment-file/:file_id', to: 'experiment_files#destroy'
 
     # assignment controller
     get '/assignment/:assignment_id', to: 'assignments#show'
-    get '/assginments/:assignment_id', to: 'assignments#index'
+    get '/assignments/:assignment_id', to: 'assignments#index'
     post '/assignment', to: 'assignments#create'
-    patch '/assignment/:assignment_id', to: 'assignments#update_information'
-    put '/assignment/:assignment_id', to: 'assignments#update_file'
+    patch '/assignment/:assignment_id', to: 'assignments#update'
     delete '/assignment/:assignment_id', to: 'assignments#destroy'
 
+    # assignment file controller
+    get '/assignment-file/:file_id', to: 'assignment_files#show'
+    get '/assignment-files/:assignment_id', to: 'assignment_files#index'
+    delete '/assignment-file/:file_id', to: 'assignment_files#destroy'
+
     # excel file controller
-    patch '/personal-excel-file/:assignment_id', to: 'excel_files#update_personal'
-    patch '/team-excel-file/:assignment_id', to: 'excel_files#update_team'
-    patch '/experiment-excel-file/:assignment_id', to: 'excel_files#update_experiment'
+    patch '/excel-file/:assignment_id', to: 'excel_files#update'
     get '/excel-file/:assignment_id', to: 'excel_files#show'
   end
 end
