d20da406813f8f5dd8997d705c2bc1d75df76334
JeongWooYeong <wjd030811@gmail.com>
2020-08-31 18:39:28 +0900

    [FIX] apply scaffold

--- app/controllers/excel_files_controller.rb
+++ app/controllers/excel_files_controller.rb
@@ -1,4 +1,6 @@
 class ExcelFilesController < ApplicationController
+  include FileScaffold::ControllerMethod
+
   before_action :jwt_required
   before_action :current_assignment
 
@@ -9,11 +11,6 @@ class ExcelFilesController < ApplicationController
   end
 
   def show
-    excel_file = @assignment.excel_file
-    return render status: :not_found unless excel_file
-
-    send_file(excel_file.path,
-              filename: excel_file.file_name,
-              status: :ok)
+    super { @assignment.excel_file }
   end
 end
