551d3237c23f107aaaf5d353c58eb2049dc66c39
JeongWooYeong <wjd030811@gmail.com>
2020-09-05 17:38:15 +0900

    [ADD] status for student to experiment file

--- app/controllers/experiment_files_controller.rb
+++ app/controllers/experiment_files_controller.rb
@@ -4,20 +4,29 @@ class ExperimentFilesController < ApplicationController
 
   before_action :jwt_required
   before_action :file_input_stream, only: :create
-  before_action :current_assignment, only: %i[create index]
-  before_action :current_student, only: %i[create destroy]
-  before_action :current_admin, only: :show
+  before_action :current_assignment, only: %i[create status_for_admin status_for_student]
+  before_action :current_student, only: %i[create destroy status_for_student]
+  before_action :current_admin, only: %i[show status_for_admin]
 
   def show
     super { ExperimentFile.find_by_id(params[:file_id]) }
   end
 
-  def index
+  def status_for_admin
     params.require(:student_id)
-    student = Student.find_by_id(params[:student_id])
 
-    super do
+    student = Student.find_by_id(params[:student_id])
+    return render status: :not_found unless student
+
+    index do
       ExperimentFile.where(student: student,
+                           assignment: @assignment)
+    end
+  end
+
+  def status_for_student
+    index do
+      ExperimentFile.where(student: @student,
                            assignment: @assignment)
     end
   end
--- spec/requests/experiment_files_request_spec.rb
+++ spec/requests/experiment_files_request_spec.rb
@@ -24,7 +24,7 @@ RSpec.describe 'ExperimentFiles', type: :request do
     end
   end
 
-  describe 'GET#index' do
+  describe 'GET#status_for_admin' do
     it 'OK' do
       request('get',
               @url_experiment_files + "/#{@assignment.id}",
@@ -37,9 +37,29 @@ RSpec.describe 'ExperimentFiles', type: :request do
 
     it 'Not Found Assignment' do
       request('get',
-              @url_experiment_files + "/#{@assignment.id + 1}",
+              @url_experiment_file + "/#{@assignment.id + 1}",
               false,
               true)
+      expect(response.status).to equal(404)
+    end
+  end
+
+  describe 'GET#status_for_student' do
+    it 'OK' do
+      request('get',
+              @url_experiment_file + "/status/#{@assignment.id}",
+              false,
+              @student_token)
+      expect(JSON.parse(response.body, symbolize_names: true))
+        .to(eql(file_information: [{ file_id: @file.id, file_name: @file.file_name }]))
+      expect(response.status).to equal(200)
+    end
+
+    it 'Not Found' do
+      request('get',
+              @url_experiment_file + "/status/#{@assignment.id + 1}",
+              false,
+              @student_token)
       expect(response.status).to equal(404)
     end
   end
