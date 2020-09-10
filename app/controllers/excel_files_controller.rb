fcb3e0aae9540461b82e8928700d4e4c2a88848a
JeongWooYeong <wjd030811@gmail.com>
2020-09-10 19:36:27 +0900

    [FIX] oredered function by show - update

--- app/controllers/excel_files_controller.rb
+++ app/controllers/excel_files_controller.rb
@@ -4,13 +4,13 @@ class ExcelFilesController < ApplicationController
   before_action :jwt_required
   before_action :current_assignment
 
+  def show
+    super { @assignment.excel_file }
+  end
+
   def update
     @assignment.generate_excel_file
 
     render status: :ok
-  end
-
-  def show
-    super { @assignment.excel_file }
   end
 end
