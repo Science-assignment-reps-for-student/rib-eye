564e40b75d666c8c200da840cc8fc4b78d428c8d
JeongWooYeong <wjd030811@gmail.com>
2020-10-26 12:58:14 +0900

    [FIX] personal file service get model

--- app/services/file/personal_file_service.rb
+++ app/services/file/personal_file_service.rb
@@ -1,11 +1,12 @@
 class PersonalFileService
-  def initialize(student_id:, assignment_id:)
+  def initialize(student_id:, assignment_id:, model:)
     @student = Student.find_by_id(student_id)
     @assignment = Assignment.find_by_id(assignment_id)
+    @model = model
   end
 
   def status
-    files = PersonalFile.where(student: @student, assignment: @assignment)
+    files = @model.where(student: @student, assignment: @assignment)
     files.map do |file|
       {
         file_name: file.file_name,
@@ -15,16 +16,16 @@ class PersonalFileService
   end
 
   def create(files, **options)
-    existing_files = PersonalFile.where(student: @student, assignment: @assignment)
+    existing_files = @model.where(student: @student, assignment: @assignment)
     conflicting_files = files.map do |file|
       existing_files.find_by_file_name(File.basename(file)).file_name
     end.compact
-    PersonalFile.create_with_file!(files, existing_files.blank?, **options)
+    @model.create_with_file!(files, existing_files.blank?, **options)
     NoticeMailer.submission(@student, @assignment).deliver_later
   end
 
   def destroy(file_id)
-    existing_file = PersonalFile.find_by_id(file_id)
+    existing_file = @model.find_by_id(file_id)
     existing_file.destroy!
   end
 end
