248f338fea8661a9d2deef46495aefdba95928a2
JeongWooYeong <wjd030811@gmail.com>
2020-10-26 15:58:25 +0900

    [ADD] LRU to management instance pool

--- app/services/service.rb
+++ app/services/service.rb
@@ -3,12 +3,13 @@ class Service
   @@instance_pool = []
 
   def self.instance(**kwargs)
-    existing_instance = @@instance_pool.find { |instance| yield(instance, kwargs) }
-    if existing_instance
-      existing_instance
+    existing_index = @@instance_pool[@@instance_pool.find_index { |instance| yield(instance, kwargs) }]
+    if existing_index
+      @@instance_pool.insert(0, @@instance_pool.delete_at(existing_index))
+      @@instance_pool[existing_index]
     else
       new_instance = new(**kwargs)
-      @@instance_pool << new_instance
+      @@instance_pool.unshift(new_instance)
       @@instance_pool.pop if @@instance_pool.length > INSTANCE_POOL_SIZE
       new_instance
     end
