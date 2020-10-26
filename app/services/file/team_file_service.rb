419baef101da597024ae1d3aeb152438e4b20805
JeongWooYeong <wjd030811@gmail.com>
2020-10-26 17:18:54 +0900

    [ADD] getter

--- app/services/file/team_file_service.rb
+++ app/services/file/team_file_service.rb
@@ -1,4 +1,6 @@
 class TeamFileService < Service
+  attr_reader :team, :assignment
+
   def initialize(student_id:, assignment_id:)
     @team = Team.find_by_student_id_and_assignment_id(student_id, assignment_id)
     @assignment = Assignment.find_by_id(assignment_id)
