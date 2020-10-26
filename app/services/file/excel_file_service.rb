b722887bb0692e82ee2a55855d73d8bbba926cfc
JeongWooYeong <wjd030811@gmail.com>
2020-10-26 17:16:10 +0900

    [ADD] override instance method

--- app/services/file/excel_file_service.rb
+++ app/services/file/excel_file_service.rb
@@ -5,6 +5,12 @@ class ExcelFileService < Service
     @assignment = Assignment.find_by_id(assignment_id)
   end
 
+  def self.instance(**kwargs)
+    super(kwargs) do |instance|
+      instance.assignment.id == kwargs[:assignment_id]
+    end
+  end
+
   def update
     excel_util = ExcelUtil.new(@assignment)
     excel_util.generate_excel_file
