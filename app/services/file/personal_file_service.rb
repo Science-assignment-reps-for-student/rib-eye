0fbc84789640228e7ac00ebe02598393bf0edfee
JeongWooYeong <wjd030811@gmail.com>
2020-10-27 09:34:22 +0900

    [FIX] personal service to static

--- app/services/file/personal_file_service.rb
+++ app/services/file/personal_file_service.rb
@@ -1,21 +1,6 @@
-class PersonalFileService < Service
-  attr_reader :student, :assignment
-
-  def initialize(student_id:, assignment_id:, model:)
-    @student = Student.find_by_id(student_id)
-    @assignment = Assignment.find_by_id(assignment_id)
-    @model = model
-  end
-
-  def self.instance(**kwargs)
-    super(kwargs) do |instance|
-      (instance.student.id == kwargs[:student_id]) &&
-        (instance.assignment.id == kwargs[:assignment_id])
-    end
-  end
-
-  def index
-    files = @model.where(student: @student, assignment: @assignment)
+class PersonalFileService
+  def self.index(model:, student_id:, assignment_id:)
+    files = model.where(student_id: student_id, assignment_id: assignment_id)
     files.map do |file|
       {
         file_name: file.file_name,
@@ -24,17 +9,22 @@ class PersonalFileService < Service
     end
   end
 
-  def create(files, **options)
-    existing_files = @model.where(student: @student, assignment: @assignment)
+  def self.create(model:, files:, **options)
+    student = Student.find_by_id(options[:student_id])
+    assignment = Assignment.find_by_id(options[:assignment_id])
+
+    existing_files = model.where(student: student, assignment: assignment)
     conflicting_files = files.map do |file|
       existing_files.find_by_file_name(File.basename(file)).file_name
     end.compact
-    @model.create_with_file!(files, existing_files.blank?, **options)
-    NoticeMailer.submission(@student, @assignment).deliver_later
+
+    model.create_with_file!(files, existing_files.blank?, **options)
+
+    NoticeMailer.submission(student, assignment).deliver_later
   end
 
-  def destroy(file_id)
-    existing_file = @model.find_by_id(file_id)
+  def self.destroy(model:, file_id:)
+    existing_file = model.find_by_id(file_id)
     existing_file.destroy!
   end
 end
