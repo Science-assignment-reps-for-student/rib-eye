e16a782a453e9fe4772e37870ffc250baa265be9
JeongWooYeong <wjd030811@gmail.com>
2020-10-27 09:46:45 +0900

    [FIX] excel service to static

--- app/services/file/excel_file_service.rb
+++ app/services/file/excel_file_service.rb
@@ -1,20 +1,9 @@
 require 'utils/excel_util'
 
-class ExcelFileService < Service
-  attr_reader :assignment
-
-  def initialize(assignment_id:)
-    @assignment = Assignment.find_by_id(assignment_id)
-  end
-
-  def self.instance(**kwargs)
-    super(kwargs) do |instance|
-      instance.assignment.id == kwargs[:assignment_id]
-    end
-  end
-
-  def update
-    excel_util = ExcelUtil.new(@assignment)
+class ExcelFileService
+  def self.update(assignment_id:)
+    assignment = Assignment.find_by_id(assignment_id)
+    excel_util = ExcelUtil.new(assignment)
     excel_util.generate_excel_file
   end
 end
