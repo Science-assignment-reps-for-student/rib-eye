0cf192d60480487ec4c5072e336b7719faa87f70
JeongWooYeong <wjd030811@gmail.com>
2020-10-27 09:49:26 +0900

    [FIX] assignment file service to static

--- app/services/file/assignment_file_service.rb
+++ app/services/file/assignment_file_service.rb
@@ -1,18 +1,7 @@
-class AssignmentFileService < Service
-  attr_reader :assignment
-
-  def initialize(assignment_id:)
-    @assignment = Assignment.find_by_id(assignment_id)
-  end
-
-  def self.instance(**kwargs)
-    super(kwargs) do |instance|
-      instance.assignment.id == kwargs[:assignment_id]
-    end
-  end
-
-  def index
-    @assignment.assignment_files.map do |file|
+class AssignmentFileService
+  def self.index(assignment_id:)
+    assignment = Assignment.find_by_id(assignment_id)
+    assignment.assignment_files.map do |file|
       {
         file_name: file.file_name,
         file_id: file.id
@@ -20,7 +9,7 @@ class AssignmentFileService < Service
     end
   end
 
-  def destroy(file_id)
+  def self.destroy(file_id:)
     existing_file = AssignmentFile.find_by_id(file_id)
     existing_file.destroy!
   end
