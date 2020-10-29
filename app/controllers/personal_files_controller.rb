4d0a94eb571ceb6b7961ca2d75af33ce16ad63df
JeongWooYeong <wjd030811@gmail.com>
2020-10-29 15:33:05 +0900

    [FIX] trim code format

--- app/controllers/personal_files_controller.rb
+++ app/controllers/personal_files_controller.rb
@@ -11,8 +11,10 @@ class PersonalFilesController < ApplicationController
   before_action :current_admin, only: %i[show status_for_admin]
 
   def show
-    params.permit(%i[file_id])
+    params.require(%i[file_id])
+
     file = PersonalFile.find_by_id(params[:file_id])
+
     send_file(file.path,
               filename: file.file_name)
   end
@@ -28,6 +30,7 @@ class PersonalFilesController < ApplicationController
 
   def status_for_student
     params.require(%i[assignment_id])
+
     render json: { file_information: index(model: PersonalFile,
                                            student_id: @student.id,
                                            assignment_id: params[:assignment_id]) },
@@ -36,17 +39,21 @@ class PersonalFilesController < ApplicationController
 
   def create
     params.require(%i[assignment_id])
+
     super(model: PersonalFile,
           files: @files,
           student_id: @payload['sub'],
           assignment_id: params[:assignment_id],
           created_at: Time.zone.now)
+
     render status: :created
   end
 
   def destroy
     params.require(%i[file_id])
+
     super(model: PersonalFile, file_id: params[:file_id])
+
     render status: :ok
   end
 end
