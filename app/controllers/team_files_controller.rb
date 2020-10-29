4f399c24b6b71afced72361babc4c92319265a24
JeongWooYeong <wjd030811@gmail.com>
2020-10-29 15:34:53 +0900

    [FIX] trim code format

--- app/controllers/team_files_controller.rb
+++ app/controllers/team_files_controller.rb
@@ -11,37 +11,45 @@ class TeamFilesController < ApplicationController
   before_action :current_admin, only: %i[show status_for_admin]
 
   def show
-    params.permit(%i[file_id])
+    params.require(%i[file_id])
+
     file = TeamFile.find_by_id(params[:file_id])
+
     send_file(file.path,
               filename: file.file_name)
   end
 
   def status_for_admin
-    params.permit(%i[team_id assignment_id])
+    params.require(%i[team_id assignment_id])
+
     render json: { file_information: index(team_id: params[:team_id],
                                            assignment_id: params[:assignment_id])},
            status: :ok
   end
 
   def status_for_student
-    params.permit(%i[assignment_id])
+    params.require(%i[assignment_id])
+
     render json: { file_information: index(team_id: @student.team(params[:assignment_id]).id,
                                            assignment_id: params[:assignment_id]) },
            status: :ok
   end
 
   def create
-    params.permit(%i[assignment_id])
+    params.require(%i[assignment_id])
+
     super(files: @files,
           team_id: @student.team(params[:assignment_id]).id,
           assignment_id: params[:assignment_id])
+
     render status: :created
   end
 
   def destroy
     params.permit(%i[file_id])
+
     super(file_id: params[:file_id])
+
     render status: :ok
   end
 end
