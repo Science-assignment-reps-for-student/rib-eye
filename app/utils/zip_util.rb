efd62ea45438e4334231f56f27ca2359da3ccf7c
JeongWooYeong <wjd030811@gmail.com>
2020-11-02 08:37:08 +0900

    [FIX] util module

--- app/utils/zip_util.rb
+++ app/utils/zip_util.rb
@@ -1,29 +1,33 @@
+# frozen_string_literal: true
+
 require 'zip'
 
-class ZipUtil
-  def initialize(assignment)
-    @assignment = assignment
-  end
-
-  def generate_compressed_file
-    FileUtils.rm_f(@assignment.compressed_file_path)
-    FileUtils.mkdir_p(File.dirname(@assignment.compressed_file_path))
-
-    Zip::File.open(@assignment.compressed_file_path, Zip::File::CREATE) do |zip|
-      search_directory_recursively(@assignment.stored_dir).each do |path|
-        zip.add(File.basename(path), path)
-      end
+module Utils
+  class ZipUtil
+    def initialize(assignment)
+      @assignment = assignment
     end
-    File.open(@assignment.compressed_file_path)
-  end
 
-  def search_directory_recursively(directory)
-    Dir.glob("#{directory}/*").map do |path|
-      if File.directory?(path)
-        search_directory_recursively(path)
-      else
-        path
+    def generate_compressed_file
+      FileUtils.rm_f(@assignment.compressed_file_path)
+      FileUtils.mkdir_p(File.dirname(@assignment.compressed_file_path))
+
+      Zip::File.open(@assignment.compressed_file_path, Zip::File::CREATE) do |zip|
+        search_directory_recursively(@assignment.stored_dir).each do |path|
+          zip.add(File.basename(path), path)
+        end
       end
-    end.flatten
+      File.open(@assignment.compressed_file_path)
+    end
+
+    def search_directory_recursively(directory)
+      Dir.glob("#{directory}/*").map do |path|
+        if File.directory?(path)
+          search_directory_recursively(path)
+        else
+          path
+        end
+      end.flatten
+    end
   end
 end
