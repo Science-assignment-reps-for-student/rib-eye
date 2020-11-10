3346270c497fff1d7aaf3a29067591232621edc6
JeongWooYeong <wjd030811@gmail.com>
2020-11-10 20:59:00 +0900

    [DELETE] helper methods

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,29 +3,6 @@ require 'jwt_base'
 class ApplicationController < ActionController::API
   @@jwt_base = JWTBase.new(ENV['SECRET_KEY_BASE'], 1.days, 2.weeks)
 
-  def current_team
-    current_student
-    @team = Team.find_by_leader_id(@student.id)
-    render status: :forbidden unless @team
-  end
-
-  def current_student
-    @student = Student.find_by_email(@payload['sub'])
-    return render status: :forbidden unless @student
-  end
-
-  def current_admin
-    @admin = Admin.find_by_email(@payload['sub'])
-    return render status: :forbidden unless @admin
-  end
-
-  def current_assignment
-    params.require(:assignment_id)
-
-    @assignment = Assignment.find_by_id(params[:assignment_id])
-    return render status: :not_found unless @assignment
-  end
-
   def jwt_required
     return render status: :unauthorized unless token
 
