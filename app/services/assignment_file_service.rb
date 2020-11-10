5a08192b608f0d45e1b0e14166da3c76c794686f
JeongWooYeong <wjd030811@gmail.com>
2020-11-10 19:56:35 +0900

    [FIX] move assignment file show business logic to service

--- app/services/assignment_file_service.rb
+++ app/services/assignment_file_service.rb
@@ -1,4 +1,12 @@
+# frozen_string_literal: true
+
 module AssignmentFileService
+  def show(file_id:)
+    file = AssignmentFile.find_by_id(file_id)
+    send_file(file.path,
+              filename: file.file_name)
+  end
+
   def index(assignment_id:)
     assignment = Assignment.find_by_id(assignment_id)
     assignment.assignment_files.map do |file|
