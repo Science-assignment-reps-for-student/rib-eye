02b91454b9e75a34d79507fe5af586d3a2a0d024
JeongWooYeong <wjd030811@gmail.com>
2020-11-11 23:28:44 +0900

    [FIX] jwt filter excel controller

--- app/controllers/excel_files_controller.rb
+++ app/controllers/excel_files_controller.rb
@@ -3,7 +3,7 @@ require './app/services/excel_file_service'
 class ExcelFilesController < ApplicationController
   include ExcelFileService
 
-  before_action :jwt_required
+  before_action :admin?
 
   def show
     params.require(%i[assignment_id])
