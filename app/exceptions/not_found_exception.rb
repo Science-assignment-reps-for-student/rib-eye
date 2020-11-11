4f0a21b73194eeaedde635d762bfa4f4cd848cf0
JeongWooYeong <wjd030811@gmail.com>
2020-11-11 23:30:57 +0900

    [FIX] add status field

--- app/exceptions/not_found_exception.rb
+++ app/exceptions/not_found_exception.rb
@@ -1,16 +1,13 @@
 module NotFoundException
   class NotFound < KeyError
-    attr_reader :should_raise
+    attr_reader :should_raise, :status
 
     def initialize(**params)
+      @status = :not_found
+
       invalid_params = params.filter { |_, value| value.nil? }.keys
       @should_raise = !invalid_params.blank?
       super("Not found : #{invalid_params}")
     end
-  end
-
-  def not_found(error)
-    render json: { error_message: error.message },
-           status: :not_found
   end
 end
