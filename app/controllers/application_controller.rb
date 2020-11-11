dc4af22da7321516c2d9a83b7d0fdf5059531239
JeongWooYeong <wjd030811@gmail.com>
2020-11-11 23:21:34 +0900

    [ADD] rescue authorization errors and require token

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,23 +3,11 @@ require 'jwt_base'
 class ApplicationController < ActionController::API
   include Exceptions
 
-  rescue_from 'NotFoundException::NotFound' do |e| not_found(e) end
+  rescue_from 'UnauthorizedException::Unauthorized' do |e| render_error(e) end
+  rescue_from 'ForbiddenException::Forbidden' do |e| render_error(e) end
+  rescue_from 'NotFoundException::NotFound' do |e| render_error(e) end
 
-  @@jwt_base = JWTBase.new(ENV['SECRET_KEY_BASE'], 1.days, 2.weeks)
-
-  def jwt_required
-    return render status: :unauthorized unless token
-
-    @payload = @@jwt_base.jwt_required(token)
-    return render status: @payload[:status] if @payload[:status]
-
-    @payload
-  end
-
-  def token
-    return nil unless request.authorization
-
-    authorization = request.authorization.split(' ')
-    authorization[1] if authorization[0] == 'Bearer'
+  before_action do
+    @payload = Exceptions.except(ForbiddenException::Forbidden, request.authorization).payload
   end
 end
