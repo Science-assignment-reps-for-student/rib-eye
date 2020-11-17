001ade75a72e2636fe110fe6cbcf5fd3285c9448
JeongWooYeong <wjd030811@gmail.com>
2020-11-17 17:16:39 +0900

    [HOTFIX] notice mailer logic

--- app/mailers/notice_mailer.rb
+++ app/mailers/notice_mailer.rb
@@ -1,17 +1,16 @@
 class NoticeMailer < ApplicationMailer
   def submission(target, assignment)
-    @name = Student.find_by_email(target.email)
+    @name = target.name
     @title = assignment.title
     @type = assignment.type_korean
     @subject = "[제출 알림][#{@type}]#{@title}"
+    @is_late = if assignment.files.last.is_late
+                 '지각'
+               else
+                 '제출'
+               end
 
     if assignment.type == 'TEAM'
-      @is_late = if assignment.files.last.is_late
-                   '지각'
-                 else
-                   '제출'
-                 end
-
       return Team.find_by_leader_id_and_assignment_id(target.id,
                                                       assignment.id)
                  .students.each do |student|
@@ -21,12 +20,6 @@ class NoticeMailer < ApplicationMailer
                     content_type: 'text/html')
              end
     end
-
-    @is_late = if assignment.files.last.is_late
-                 '지각'
-               else
-                 '제출'
-               end
 
     mail(from: 'notify@scarfs.hs.kr',
          to: target.email,
